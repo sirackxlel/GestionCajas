2bf8e42a8b1d53a95ceec932f086313d76f5686e
sirackxlel <sirackxlel@gmail.com>
2025-07-05 09:53:36 -0300

    Asignacion automatica de cajas

--- entidades/models.py
+++ entidades/models.py
@@ -16,6 +16,7 @@ class Proceso(models.Model):
 class Entidad(models.Model):
     nombre = models.CharField(max_length=100)
     proceso = models.ForeignKey(Proceso, on_delete=models.CASCADE)
+    activa = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
