d98ab9d491813fb41b7f9771b2f31650749860b0
sirackxlel <sirackxlel@gmail.com>
2025-07-08 17:32:54 -0300

    cambio a mysql

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gestion_cajas',
+        'USER': 'django_user',
+        'PASSWORD': 'nachi',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
